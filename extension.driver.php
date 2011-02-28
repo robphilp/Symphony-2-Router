11acd2c83dc91b7314efa5d3b0b4701e56b5c7bc
Max Wheeler <max@makenosound.com>
2011-03-01 10:19:00 +1100

    Fixed space/version number.

--- extension.driver.php
+++ extension.driver.php
@@ -1,19 +1,19 @@
 <?php
 
-	Class Extension_Router extends Extension{
+  Class Extension_Router extends Extension{
 
-		public function about() {
-			return array('name' => 'URL Router',
-						 'version' => '0.3',
-						 'release-date' => '2010-04-07',
-						 'author' => array('name' => 'Robert Philp',
-										   'website' => 'http://robertphilp.com',
-										   'email' => ''),
-							'description'   => 'Allows URL routing for vanity URLs etc'
-				 		);
-		}
+    public function about() {
+      return array('name' => 'URL Router',
+             'version' => '0.4',
+             'release-date' => '2010-08-31',
+             'author' => array('name' => 'Robert Philp',
+                       'website' => 'http://robertphilp.com',
+                       'email' => ''),
+              'description'   => 'Allows URL routing for vanity URLs etc'
+             );
+    }
 
-		public function install() {
+    public function install() {
             $this->_Parent->Database->query("
                     CREATE TABLE IF NOT EXISTS `tbl_router` (
                         `id` int(11) NOT NULL auto_increment,
@@ -28,149 +28,149 @@
             $this->_Parent->Database->query("DROP TABLE `tbl_router`");
         }
 
-		
-		public function getSubscribedDelegates() {
-			return array(
-				array(
-					'page'		=> '/frontend/',
-					'delegate'	=> 'FrontendPrePageResolve',
-					'callback'	=> 'frontendPrePageResolve'
-				),
-				array(
-					'page'		=> '/system/preferences/',
-					'delegate'	=> 'AddCustomPreferenceFieldsets',
-					'callback'	=> 'addCustomPreferenceFieldsets'
-				),
-				array(
-					'page'      => '/system/preferences/',
-					'delegate'  => 'Save',
-					'callback'  => 'save'
-				),
-			);
-		}
+    
+    public function getSubscribedDelegates() {
+      return array(
+        array(
+          'page'    => '/frontend/',
+          'delegate'  => 'FrontendPrePageResolve',
+          'callback'  => 'frontendPrePageResolve'
+        ),
+        array(
+          'page'    => '/system/preferences/',
+          'delegate'  => 'AddCustomPreferenceFieldsets',
+          'callback'  => 'addCustomPreferenceFieldsets'
+        ),
+        array(
+          'page'      => '/system/preferences/',
+          'delegate'  => 'Save',
+          'callback'  => 'save'
+        ),
+      );
+    }
 
-		public function getRoutes() {
-			$routes = $this->_Parent->Database->fetch("SELECT * FROM tbl_router");
-			return $routes;
+    public function getRoutes() {
+      $routes = $this->_Parent->Database->fetch("SELECT * FROM tbl_router");
+      return $routes;
         }
 
-		public function save($context) {
-			$routes = array();
+    public function save($context) {
+      $routes = array();
 
-			if ($context['settings']['router']['routes']) {
-				$route = array();
-				foreach($context['settings']['router']['routes'] as $item) {
-					if(isset($item['from'])) {
-						$route['from'] = $item['from'];
-					} else if(isset($item['to'])) {
-						$route['to'] = $item['to'];
-						$routes[] = $route;
-						$route = array();
-					}
-				}
-			}
+      if ($context['settings']['router']['routes']) {
+        $route = array();
+        foreach($context['settings']['router']['routes'] as $item) {
+          if(isset($item['from'])) {
+            $route['from'] = $item['from'];
+          } else if(isset($item['to'])) {
+            $route['to'] = $item['to'];
+            $routes[] = $route;
+            $route = array();
+          }
+        }
+      }
 
-			$this->_Parent->Database->query("DELETE FROM tbl_router");
+      $this->_Parent->Database->query("DELETE FROM tbl_router");
 
-			if (count($routes) != 0) {
-				$this->_Parent->Database->insert($routes, "tbl_router");
-				unset($context['settings']['router']['routes']);
-			}
-			
-			if(!is_array($context['settings'])) $context['settings'] = array('router' => array('redirect' => 'no'));
-			
-			elseif(!isset($context['settings']['router']['redirect'])){
-				$context['settings']['router'] = array('redirect' => 'no');
-			}
-		}
+      if (count($routes) != 0) {
+        $this->_Parent->Database->insert($routes, "tbl_router");
+        unset($context['settings']['router']['routes']);
+      }
+      
+      if(!is_array($context['settings'])) $context['settings'] = array('router' => array('redirect' => 'no'));
+      
+      elseif(!isset($context['settings']['router']['redirect'])){
+        $context['settings']['router'] = array('redirect' => 'no');
+      }
+    }
 
-		public function addCustomPreferenceFieldsets($context){
-			$fieldset = new XMLElement('fieldset');
-			$fieldset->setAttribute('class', 'settings');
-			$fieldset->appendChild(new XMLElement('legend', 'Regex URL re-routing'));
+    public function addCustomPreferenceFieldsets($context){
+      $fieldset = new XMLElement('fieldset');
+      $fieldset->setAttribute('class', 'settings');
+      $fieldset->appendChild(new XMLElement('legend', 'Regex URL re-routing'));
  
-			$p = new XMLElement('p', 'Define regex rules for URL re-routing');
-			$p->setAttribute('class', 'help');
-			$fieldset->appendChild($p);
+      $p = new XMLElement('p', 'Define regex rules for URL re-routing');
+      $p->setAttribute('class', 'help');
+      $fieldset->appendChild($p);
 
-			$group = new XMLElement('div');
-			$group->setAttribute('class', 'subsection');
-			$group->appendChild(new XMLElement('h3', __('URL Schema Rules')));
+      $group = new XMLElement('div');
+      $group->setAttribute('class', 'subsection');
+      $group->appendChild(new XMLElement('h3', __('URL Schema Rules')));
  
-			$ol = new XMLElement('ol');
-			$ol->setAttribute('id', 'router');
-			$li = new XMLElement('li');
-			$li->setAttribute('class', 'template');
-			$labelfrom = Widget::Label(__('From'));
-           	$labelfrom->appendChild(Widget::Input("settings[router][routes][][from]", "From"));
+      $ol = new XMLElement('ol');
+      $ol->setAttribute('id', 'router');
+      $li = new XMLElement('li');
+      $li->setAttribute('class', 'template');
+      $labelfrom = Widget::Label(__('From'));
+      $labelfrom->appendChild(Widget::Input("settings[router][routes][][from]", "From"));
+      $labelto = Widget::Label(__('To'));
+      $labelto->appendChild(Widget::Input("settings[router][routes][][to]", "To"));
+      $divgroup = new XMLElement('div');
+      $divgroup->setAttribute('class', 'group');
+      $divgroup->appendChild($labelfrom);
+      $divgroup->appendChild($labelto);
+  
+      $divcontent = new XMLElement('div');
+      $divcontent->setAttribute('class', 'content');
+      $divcontent->appendChild($divgroup);      
+
+      $li->appendChild(new XMLElement('h4', "Route"));
+      $li->appendChild($divcontent);
+      $ol->appendChild($li);
+      if($routes = $this->getRoutes()) {
+        if(is_array($routes)) {
+          $i = 1;
+          foreach($routes as $route) {
+            $li = new XMLElement('li');
+            $li->setAttribute('class', 'instance expanded');
+            $h4 = new XMLElement('h4', 'Route');
+            //$h4->appendChild(new XMLElement('span', 'Route'));
+            $li->appendChild($h4);
+            $divcontent = new XMLElement('div');
+            $divcontent->setAttribute('class', 'content');
+            $divgroup = new XMLElement('div');
+            $divgroup->setAttribute('class', 'group');
+            $labelfrom = Widget::Label(__('From'));
+            $labelfrom->appendChild(Widget::Input("settings[router][routes][][from]", General::sanitize($route['from'])));
             $labelto = Widget::Label(__('To'));
-            $labelto->appendChild(Widget::Input("settings[router][routes][][to]", "To"));
-			$divgroup = new XMLElement('div');
-			$divgroup->setAttribute('class', 'group');
-			$divgroup->appendChild($labelfrom);
-			$divgroup->appendChild($labelto);
-	
-			$divcontent = new XMLElement('div');
-			$divcontent->setAttribute('class', 'content');
-			$divcontent->appendChild($divgroup);			
+            $labelto->appendChild(Widget::Input("settings[router][routes][][to]", General::sanitize($route['to'])));
+            $divgroup->appendChild($labelfrom);
+            $divgroup->appendChild($labelto);
+            $divcontent->appendChild($divgroup);
+            $li->appendChild($divcontent);
+            $ol->appendChild($li);
+            $i++;
+          }
+        }
+      }
+      $group->appendChild($ol);
+      
+      $label = Widget::Label();
+      $input = Widget::Input('settings[router][redirect]', 'yes', 'checkbox');
+      if($this->_Parent->Configuration->get('redirect', 'router') == 'yes') $input->setAttribute('checked', 'checked');
+      $label->setValue($input->generate() . ' ' . __('Redirect legacy URLs to new destination'));
+      $fieldset->appendChild($label);
+      
+      $fieldset->appendChild(new XMLElement('p', __('Redirects requests to the new destination instead of just displaying the content under the legacy URL.'), array('class' => 'help')));
+      
+      $fieldset->appendChild($group);
+      $context['wrapper']->appendChild($fieldset);  
+    }
 
-			$li->appendChild(new XMLElement('h4', "Route"));
-			$li->appendChild($divcontent);
-			$ol->appendChild($li);
-			if($routes = $this->getRoutes()) {
-				if(is_array($routes)) {
-					$i = 1;
-					foreach($routes as $route) {
-						$li = new XMLElement('li');
-						$li->setAttribute('class', 'instance expanded');
-						$h4 = new XMLElement('h4', 'Route');
-						//$h4->appendChild(new XMLElement('span', 'Route'));
-						$li->appendChild($h4);
-						$divcontent = new XMLElement('div');
-						$divcontent->setAttribute('class', 'content');
-						$divgroup = new XMLElement('div');
-						$divgroup->setAttribute('class', 'group');
-						$labelfrom = Widget::Label(__('From'));
-						$labelfrom->appendChild(Widget::Input("settings[router][routes][][from]", General::sanitize($route['from'])));
-						$labelto = Widget::Label(__('To'));
-						$labelto->appendChild(Widget::Input("settings[router][routes][][to]", General::sanitize($route['to'])));
-						$divgroup->appendChild($labelfrom);
-						$divgroup->appendChild($labelto);
-						$divcontent->appendChild($divgroup);
-						$li->appendChild($divcontent);
-						$ol->appendChild($li);
-						$i++;
-					}
-				}
-			}
-			$group->appendChild($ol);
-			
-			$label = Widget::Label();
-			$input = Widget::Input('settings[router][redirect]', 'yes', 'checkbox');
-			if($this->_Parent->Configuration->get('redirect', 'router') == 'yes') $input->setAttribute('checked', 'checked');
-			$label->setValue($input->generate() . ' ' . __('Redirect legacy URLs to new destination'));
-			$fieldset->appendChild($label);
-			
-			$fieldset->appendChild(new XMLElement('p', __('Redirects requests to the new destination instead of just displaying the content under the legacy URL.'), array('class' => 'help')));
-			
-			$fieldset->appendChild($group);
-			$context['wrapper']->appendChild($fieldset);	
-		}
-
-		public function frontendPrePageResolve($context) {
-			$routes = $this->getRoutes();
-			$url = $context['page'];
-			foreach($routes as $route) {
-				if(preg_match($route['from'], $url, $matches) == 1) {
-					$new_url = preg_replace($route['from'], $route['to'], $url);
-					if($this->_Parent->Configuration->get('redirect', 'router') == 'yes') {
-						header("Location:" . $new_url);
-						die();
-					}
-					break;
-				}
-			}
-			if($new_url) $context['page'] = $new_url;
-		}
-			
-	}
+    public function frontendPrePageResolve($context) {
+      $routes = $this->getRoutes();
+      $url = $context['page'];
+      foreach($routes as $route) {
+        if(preg_match($route['from'], $url, $matches) == 1) {
+          $new_url = preg_replace($route['from'], $route['to'], $url);
+          if($this->_Parent->Configuration->get('redirect', 'router') == 'yes') {
+            header("Location:" . $new_url);
+            die();
+          }
+          break;
+        }
+      }
+      if($new_url) $context['page'] = $new_url;
+    }
+      
+  }
